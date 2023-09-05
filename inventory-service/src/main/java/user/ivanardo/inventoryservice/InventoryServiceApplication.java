1809d1464ec8fa7e645220d03ca4a967234c950f
ivanardo <ivanardoo@gmail.com>
2023-09-05 15:47:51 +0300

    Service Discovery

--- inventory-service/src/main/java/user/ivanardo/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/user/ivanardo/inventoryservice/InventoryServiceApplication.java
@@ -3,6 +3,7 @@ package user.ivanardo.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import user.ivanardo.inventoryservice.model.Inventory;
 import user.ivanardo.inventoryservice.repository.InventoryRepository;
