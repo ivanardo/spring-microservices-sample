01444b6ca3ebb8711a49226e373fbe30ffcc7f70
ivanardo <ivanardoo@gmail.com>
2023-09-08 19:06:27 +0300

    remove unused import

--- discovery-server/src/main/java/user/ivanardo/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/user/ivanardo/discoveryserver/config/SecurityConfig.java
@@ -12,8 +12,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.function.Function;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
